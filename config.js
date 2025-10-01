84920263d6a4b4e515bb0d7b1f3b72a51ebb18e4
chris-t-moran <chris.t.moran@gmail.com>
2025-10-01 13:15:52 +0100

    Removing ENV variables from Main.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  supabase: {
+    url: 'https://vqkoapgiozhytoqscxxx.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa29hcGdpb3poeXRvcXNjeHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjEzMjIsImV4cCI6MjA2MTIzNzMyMn0.z4h2_uY-VlprsvaRZElh3ZOiGHG-fpGHO5rd7Y2nssY'
+  }
+};
