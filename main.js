1dee881f7ec54fb7f761b8b80162b9015a140124
chris-t-moran <chris.t.moran@gmail.com>
2025-10-01 13:17:47 +0100

    Removing ENV variables

--- main.js
+++ main.js
@@ -9,20 +9,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Configuration & State
 // =============================================================================
 
-const CONFIG = {
-  supabase: {
-    url: 'https://vqkoapgiozhytoqscxxx.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa29hcGdpb3poeXRvcXNjeHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjEzMjIsImV4cCI6MjA2MTIzNzMyMn0.z4h2_uY-VlprsvaRZElh3ZOiGHG-fpGHO5rd7Y2nssY'
-  },
-  map: {
-    mobile: { center: [53.36873328276553, -6.258910850717367], zoom: 15 },
-    desktop: { center: [53.37155, -6.25873], zoom: 16 }
-  },
-  view: {
-    ttlHours: 24,
-    clusterRadius: 40
-  }
-};
+import { CONFIG as IMPORTED_CONFIG } from './config.js';
+const CONFIG = IMPORTED_CONFIG;
 
 const state = {
   db: createClient(CONFIG.supabase.url, CONFIG.supabase.key),
