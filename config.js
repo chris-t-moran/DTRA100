1b530ac961040f4a70d11f5415cfa49db590d88c
chris-t-moran <chris.t.moran@gmail.com>
2025-10-01 13:18:11 +0100

    Updated config

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 export const CONFIG = {
-  supabase: {
+   supabase: {
     url: 'https://vqkoapgiozhytoqscxxx.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa29hcGdpb3poeXRvcXNjeHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjEzMjIsImV4cCI6MjA2MTIzNzMyMn0.z4h2_uY-VlprsvaRZElh3ZOiGHG-fpGHO5rd7Y2nssY'
+  },
+  map: {
+    mobile: { center: [53.36873328276553, -6.258910850717367], zoom: 15 },
+    desktop: { center: [53.37155, -6.25873], zoom: 16 }
+  },
+  view: {
+    ttlHours: 24,
+    clusterRadius: 40
   }
 };
